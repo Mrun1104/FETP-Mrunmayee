78b3268eabd8f20c4b203bcf7a265fac72616c47
Mrun1104 <85399380+Mrun1104@users.noreply.github.com>
2023-11-10 18:45:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,90 +1,57 @@
-import email
-from flask import Flask, render_template, request, redirect, url_for, session
-import datetime
-import oauthlib
-import pytz
-from flask_oauthlib.client import OAuth
+from flask import Flask, render_template, url_for, redirect, session
+from authlib.integrations.flask_client import OAuth
+import google
+import os
+import random
+import string
 
-app = Flask(__name__)
-app.secret_key = 'your_secret_key'
-oauth = oauthlib(app)
+def generate_nonce(length=32):
+    characters = string.ascii_letters + string.digits
+    t = ''.join(random.choice(characters) for _ in range(length))
+    return t
+	
+app = Flask(__name__, template_folder='template')
+app.secret_key = 'm7558671042a7020712503p9226933968'
 
-@app.route('/')
-def login():
-    return render_template('login.html')
-
-@app.route('/authenticate', methods=['POST'])
-def authenticate():
-    email = request.form['email']
-
-google = oauth.remote_app(
-    'google',
-    consumer_key='763754207706-b52b5nn06ajtmkpogom6g1t0u2qr4d2c.apps.googleusercontent.com',
-    consumer_secret='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',
-    request_token_params={
-        'scope': 'https://www.googleapis.com/auth/gmail.readonly',
-    },
-    base_url='https://www.googleapis.com/gmail/v1/',
-    request_token_url=None,
-    access_token_method='POST',
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-)
+app.config['SERVER_NAME'] = 'localhost:5000'
+oauth = OAuth(app)
 
 @app.route('/')
-def login():
-    return render_template('login.html')
+def index():
+	return render_template('index.html')
 
-@app.route('/login')
-def google_login():
-    return google.authorize(callback=url_for('authorized', _external=True))
+@app.route('/google/')
+def google():
+	GOOGLE_CLIENT_ID = os.environ.get('763754207706-b52b5nn06ajtmkpogom6g1t0u2qr4d2c.apps.googleusercontent.com')
+	GOOGLE_CLIENT_SECRET = os.environ.get('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')
 
-@app.route('/logout')
-def logout():
-    session.pop('google_token', None)
-    return redirect(url_for('login'))
+	nonce = generate_nonce()
+	session['nonce'] = nonce
+	CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+	oauth.register(
+		name='google',
+		client_id='763754207706-b52b5nn06ajtmkpogom6g1t0u2qr4d2c.apps.googleusercontent.com',
+		client_secret='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',
+		server_metadata_url=CONF_URL,
+		client_kwargs={
+			'scope': 'openid email profile'
+		}
+	)
+	
+	# Redirect to google_auth function
+	redirect_uri = url_for('google_auth', _external=True)
+	auth_url = oauth.google.create_authorization_url(redirect_uri, nonce=nonce)
 
-@app.route('/login/authorized')
-def authorized():
-    response = google.authorized_response()
-    if response is None or response.get('access_token') is None:
-        return 'Access denied: reason={} error={}'.format(
-            request.args['error_reason'],
-            request.args['error_description']
-        )
+	return auth_url
 
-    session['google_token'] = (response['access_token'], '')
-    user_info = google.get('https://www.googleapis.com/gmail/v1/users/me/profile').data
-
-    session['email'] = user_info['emailAddress']
-    session['name'] = user_info['displayName']
-
-    return redirect(url_for('success'))
-
-@google.tokengetter
-def get_google_oauth_token():
-    return session.get('google_token')
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-   
-    session['email'] = "email"
-    session['name'] = "name"
-
-    
-
-@app.route('/success')
-def success():
-    if 'email' in session:
-        return render_template('success.html')
-    else:
-        return redirect(url_for('login'))
-    
-def get_indian_time():
-    india_timezone = pytz.timezone('Asia/Kolkata')
-    time_in_india = datetime.datetime.now(india_timezone)
-    return time_in_india.strftime('%Y-%m-%d %H:%M:%S %Z%z')
+@app.route('/google/auth/')
+def google_auth():
+    token = oauth.google.authorize_access_token()
+    nonce = session.get('nonce')
+    user = oauth.google.parse_id_token('https://oauth2.googleapis.com/token', nonce=nonce)
+    print("Google User: ", user)
+    return redirect('/')
 
 
-
+if __name__ == "__main__":
+	app.run(debug=True)
